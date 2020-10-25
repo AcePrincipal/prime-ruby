64a95930a01face7b6262ccf8e4ee503bcbbe1d8
Alexander <acepetrov96@gmail.com>
2020-10-25 18:22:51 -0500

    commit tree b86b57e146ec902bdf2d0669d864ad1eef3f3e7c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+    if num < 2
+        return false
+    end
+
+    range = (2..(num-1)).to_a
+    range.each do |test|
+        if num % test == 0
+            return false
+        elsif num == 2 
+            return true
+        end
+    end
+
+    return true
+end
